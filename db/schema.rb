6870291e746dff1ccbf62cac92a876d03fc9ec33
Yazima <yazima04200@gmail.com>
2023-03-01 20:30:59 +0900

    画像添付機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_090404) do
+ActiveRecord::Schema.define(version: 2023_03_01_112448) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
